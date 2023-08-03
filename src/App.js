1426073563608f3c55a22fc83d9fc01b8c552c1d
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-08-03 16:46:46 +0300

    Commit 1
    Add 2 button.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from "react";
 
 function App() {
+
+  let likes = 9;
+
+  function likesSea () {
+    if(likes === -1){
+      likes = 0
+    }
+  }
   return (
+
     <div className="App">
-      
+
+      <button onClick={() => {
+        likes=likes+1;
+        likesSea();
+        console.log(likes);
+      }}>Increment</button>
+
+      <button  onClick={() => {
+        likes=likes-1;
+        likesSea();
+        console.log(likes);
+      }}>Decrement</button>
+
     </div>
   );
 }
